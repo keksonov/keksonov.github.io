eb41e43467fc55b77ffea597e84fc8572c657732
Ilya Kulgavy <i.kulgavy@gmail.com>
2017-05-31 21:18:34 +0300

    added new site

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	var video = $('.adaptive-video');
+	var width = video.width();
+	var heigth = width * 0.5625;
+	video.height(heigth);
+
+	$('.fa-bars').on('click', function() {
+		$('.menu-for-mobile').toggleClass('active');
+	})
+})
