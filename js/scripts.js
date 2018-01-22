2179a2d72df95183984380d3e7da4484c2a160cd
Ilya Kulgavy <i.kulgavy@gmail.com>
2018-01-22 19:34:28 +0300

    portfolio-fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 (function () {
   var header = document.getElementsByClassName('header')[0]
-  console.log(header)
   window.addEventListener('scroll', function () {
     var scroll = window.scrollY;
 
-    console.log(window.innerHeight - scroll)
-    header.style.height = window.innerHeight - scroll + 'px';
+    if (scroll < window.innerHeight - scroll) {
+      header.style.height = window.innerHeight - scroll + 'px';
+    }
   });
 }());
